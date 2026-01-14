1319477e9532193cb3ba52984c801ee4364ef77b
Kajih <itskajih@gmail.com>
2026-01-14 01:37:17 +0100

    try to improve scrolling in search tab on phone

--- lib/hooks/useTierListDnD.ts
+++ lib/hooks/useTierListDnD.ts
@@ -42,7 +42,11 @@ export function useTierListDnD(
   const [overId, setOverId] = useState<string | null>(null);
 
   const sensors = useSensors(
-    useSensor(PointerSensor),
+    useSensor(PointerSensor, {
+        activationConstraint: {
+            distance: 8,
+        },
+    }),
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
     })
