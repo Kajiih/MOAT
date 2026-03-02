58a98da29eeb71a93d6dd646279af1cc2fd6c736
Kajih <itskajih@gmail.com>
2026-03-02 01:21:35 +0100

    Adding remove files

--- lib/services/musicbrainz/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @file index.ts
- * @description Public API for the MusicBrainz Service module.
- * @module MusicBrainzService
- */
-
-export * from './config';
-export * from './details';
-export * from './search';
